f9af6f67e30e3f8cb7f506cf0029692072dfdb81
Annabelle Kempster <annabellekempster@Annabelles-MacBook-Pro.local>
2018-10-07 01:53:16 -0400

    fixing bug & updating appearance

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 var URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
-var mapboxKey = "pk.eyJ1IjoiYWtlbXBzdGVyIiwiYSI6ImNqbThjbDlxaTB5YzQzcXFxZXkxYWx1eWwifQ.nef5A_ZVogRvCzqPwJWVtg"
+var mapboxToken = "pk.eyJ1IjoiYWtlbXBzdGVyIiwiYSI6ImNqbThjbDlxaTB5YzQzcXFxZXkxYWx1eWwifQ.nef5A_ZVogRvCzqPwJWVtg"
 var mapboxURL = "https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?"
 
 d3.json(URL, function (data) {
@@ -8,10 +8,10 @@ d3.json(URL, function (data) {
 
 function chooseColor(magnitude) {
     if (magnitude < 1) {
-        return "#4FFF2F"
+        return "#0c905d"
     }
     else if (magnitude < 2) {
-        return "#BAFF2F"
+        return "#97e200"
     }
     else if (magnitude < 3) {
         return "#FFC300"
@@ -20,44 +20,40 @@ function chooseColor(magnitude) {
         return "#FF5733"
     }
     else if (magnitude < 5) {
-        return "#C70039"
+        return "#c70007"
     }
     else if (magnitude < 6) {
-        return "#900C3F"
+        return "#900c1e"
     }
     else {
         return "#581845"
     };
 };
 
-function createFeatures(earthquakeData) {
+function createFeatures(data) {
     function onEachFeature(feature, layer) {
         var months = ['Jan','Feb','Mar','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-        var days = ['Sun','Mon','Tues','Wed','Thu','Fri','Sat']
-        var timestamp = new Date(feature.properties.time);
-        var day = days[timestamp.getDay()];
-        var month = months[timestamp.getMonth()];
-        var date = timestamp.getDate();
-        var hours = timestamp.getHours();
-        var minutes = "0" + timestamp.getMinutes();
-        var formattedTime = day + ", " + month + " " + date + " " + hours + ':' + minutes.substr(-2);
+        var days = ['Sun','Mon','Tues','Wed','Thurs','Fri','Sat']
+
+
+        var quakeTime = new Date(feature.properties.time);
 
         layer.bindPopup(`<strong>Location: ${feature.properties.place}</strong>
             <hr>
             Magnitude: ${feature.properties.mag}
             <br>
-            Time: ${formattedTime}`);
+            Time: ${quakeTime}`);
     };
 
-    var earthquakes = L.geoJSON(earthquakeData, {
+    var earthquakes = L.geoJSON(data, {
         pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng, {
-                radius: Math.pow(2,feature.properties.mag),
+                radius: Math.pow(1.88,feature.properties.mag),
                 fillColor: chooseColor(feature.properties.mag),
                 color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
+                weight: .2,
+                opacity: .7,
+                fillOpacity: 0.7
             });
         },
         onEachFeature: onEachFeature
@@ -66,24 +62,24 @@ function createFeatures(earthquakeData) {
 };
 
 function createMap(earthquakes) {
-    var streetmap = L.tileLayer(`${mapboxURL}access_token=${mapboxKey}`);
+    var map = L.tileLayer(`${mapboxURL}access_token=${mapboxToken}`);
     var myMap = L.map("map", {
         center: [20, -20],
-        zoom: 2.5,
-        layers: [streetmap, earthquakes]
+        zoom: 2.9,
+        layers: [map, earthquakes]
     });
     var legend = L.control({ position: 'bottomright' });
 
     legend.onAdd = function (map) {
 
         var div = L.DomUtil.create('div', 'info legend'),
-            grades = [0, 1, 2, 3, 4, 5, 6],
+            intensity = [0, 1, 2, 3, 4, 5, 6],
             labels = [];
 
-        for (var i = 0; i < grades.length; i++) {
+        for (var i = 0; i < intensity.length; i++) {
             div.innerHTML +=
-                '<i style="background:' + chooseColor(grades[i]) + '"></i> ' +
-                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+                '<i style="background:' + chooseColor(intensity[i]) + '"></i> ' +
+                intensity[i] + (intensity[i + 1] ? '&ndash;' + intensity[i + 1] + '<br>' : '+');
         }
 
         return div;
